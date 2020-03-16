ec4a7c1e0a83911b09782731555e2ca008112328
Raman N <roma.nareiko@gmail.com>
2020-03-17 00:10:48 +0300

    Made the header interactive

---
+++ script.js
@@ -0,0 +1,30 @@
+!!function() {
+    document.addEventListener("DOMContentLoaded", () => {
+        //Header
+        const header = document.getElementById("header");
+        let sticky = header.offsetTop;
+        const stickyHeaderClass = "header_sticky";
+        const activeMenuClass = "menu__link_active";
+        const menu = {};
+
+        window.onscroll = function(e) {
+            if (window.pageYOffset > sticky) {
+                header.classList.add(stickyHeaderClass);
+            } else {
+                header.classList.remove(stickyHeaderClass);
+            }
+        };
+
+        [...document.getElementsByClassName("menu__link")].forEach(link => {
+            link.addEventListener("click", function() {
+                let active = document.getElementsByClassName(activeMenuClass)[0];
+
+                if(active !== undefined) {
+                    active.classList.remove(activeMenuClass);
+                }
+
+                this.classList.add(activeMenuClass);
+            });
+        });
+    });
+}();
