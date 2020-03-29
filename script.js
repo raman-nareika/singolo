a6d079a489274157a138fbeaa16fbd4cebca445b
Raman N <roma.nareiko@gmail.com>
2020-03-29 23:51:46 +0300

    wip

--- script.js
+++ script.js
@@ -49,13 +49,77 @@
             });
         };
 
+        document.querySelector(".header__burger").onclick = function() {
+            this.classList.toggle("rotated90");
+            document.querySelector(".header__title").classList.toggle("header__title_left");
+            document.querySelector("nav").classList.toggle("nav_active");
+        };
+
         //slider
 
         [...document.getElementsByClassName("slider__phone_home")].forEach(btn => {
             btn.onclick = function(e) {
-                e.target.parentNode.querySelector(".slider__phone-display").classList.toggle("active");
+                e.target.parentNode.querySelector(".slider__phone-display").classList.toggle("slider__phone_active");
             }
         });
+
+        let items = document.querySelectorAll('.slider__slide');
+        let currentItem = 0;
+        let isEnabled = true;
+
+        const changeCurrentItem = function(n) {
+            currentItem = (n + items.length) % items.length;
+        }
+
+        const changeSliderBackground = function () {
+            const slider = document.getElementById("slider");
+            
+            slider.classList.toggle("slider-red");
+            slider.classList.toggle("slider-blue");
+        };
+
+        function hideItem(direction) {
+            isEnabled = false;
+            items[currentItem].classList.add(direction);
+            items[currentItem].addEventListener('animationend', function() {
+                this.classList.remove('slider__slide_active', direction);
+            });
+        }
+
+        function showItem(direction) {
+            items[currentItem].classList.add('slider__slide_next', direction);
+            items[currentItem].addEventListener('animationend', function() {
+                this.classList.remove('slider__slide_next', direction);
+                this.classList.add('slider__slide_active');
+                isEnabled = true;
+            });
+        }
+
+        function nextItem(n) {
+            hideItem('to-left');
+            changeCurrentItem(n + 1);
+            changeSliderBackground();
+            showItem('from-right');
+        }
+
+        function previousItem(n) {
+            hideItem('to-right');
+            changeCurrentItem(n - 1);
+            changeSliderBackground();
+            showItem('from-left');
+        }
+
+        document.querySelector(".slider__prev").onclick = function() {
+            if (isEnabled) {
+                previousItem(currentItem);
+            }
+        };
+
+        document.querySelector(".slider__next").onclick = function() {
+            if (isEnabled) {
+                nextItem(currentItem);
+            }
+        };
 
         //Portfolio
         
